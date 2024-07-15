1d4d5b35e4e13937fb6d8430a7ab813f1bc82c61
Martin <martineprocopio2020@gmail.com>
2024-07-14 21:35:40 -0300

    Coneccion a Api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react';
+import Layout from './components/Layout';
+import { Route,Routes } from 'react-router-dom';
+
+const apikey = '05a204294453d9d18689f24088233147';
+const ts = '29/07/2022, 13:52:07';
+const hash = '2402a1413666472ba43491dda4a70a8c';
+const URL = `http://gateway.marvel.com/v1/public/series?ts=${ts}&apikey=${apikey}&hash=${hash}`
 
 function App() {
+  
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () =>{
+    
+    try
+    {
+
+      const response = await fetch(URL).then(response=>response.json());
+      setMovies(response.data.results);
+      console.log(response.data.results);
+
+    } 
+    catch(err)
+    {
+      console.log(err);
+    }
+  }
+
+
+
+  useEffect(() => {
+    getMovies();
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
