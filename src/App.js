c910c980b017e119e0862810949240c059ae7c94
Martin <martineprocopio2020@gmail.com>
2024-07-14 21:45:56 -0300

    Coneccion a Api

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const apikey = '05a204294453d9d18689f24088233147';
 const ts = '29/07/2022, 13:52:07';
 const hash = '2402a1413666472ba43491dda4a70a8c';
 const URL = `http://gateway.marvel.com/v1/public/series?ts=${ts}&apikey=${apikey}&hash=${hash}`
+const api = 123;
 
 function App() {
   
